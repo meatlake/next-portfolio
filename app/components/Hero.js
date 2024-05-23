69ec2f6ddaee29026d806ba73e676a22a2965453
meatlake <165269007+meatlake@users.noreply.github.com>
2024-05-23 15:52:14 +0200

    Small margin change

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="mt-4 p-12 bg-gradient-to-tl from-sky-600 to-sky-400 rounded-xl min-h-28 text-white">
+    <div className="mt-4 mb-12 p-12 bg-gradient-to-tl from-sky-600 to-sky-400 rounded-xl min-h-28 text-white">
       <div className="max-w-screen-md">
         <h1 className="mb-4 text-4xl font-semibold">Portfolio sprint 3</h1>
         <p>
